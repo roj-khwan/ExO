07dd184c362bdebd8b740d8521ef9de6b10fc46f
Kanompan <83032267+roj-khwan@users.noreply.github.com>
2024-06-04 21:18:04 +0700

    update game.py to cath up with update and add some agent opinion for reference

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pygame
 import Tools as exo
+from Models.Genetic import Agent
 
 SCREEN_SIZE = 450
 GRID_SIZE = SCREEN_SIZE / 3
@@ -70,6 +71,10 @@ if __name__ == "__main__":
                      LINE_WIDTH)
     #endregion Screen
 
+    agent = Agent(
+        '14030186 19001187 18030673 02021661 14150485 12191321 17061728 01110658 13041223 08161315 04070232 03191107 13150172 12000723 14050369 06081660 03190690 13151559 03181396 05141064 17060332 14010492 06141287 06031029 11020365 03030166 19071673 03100446 07101282 14161126 15170432 11151713',
+        1)
+
     run = True
     win = 0
     while run:
@@ -80,12 +85,14 @@ if __name__ == "__main__":
                 break
 
             if event.type == pygame.MOUSEBUTTONDOWN and win == 0:
+                #get mouse pos turn into grid pos
                 posx, posy = pygame.mouse.get_pos()
                 pos = ((posx - OFFSET) // GRID_SIZE, (posy - OFFSET) // GRID_SIZE)
 
                 pos = (int(max(min(pos[0], 2), 0)), int(max(min(pos[1], 2), 0)))
 
-                if exo.CheckEmpty(board, pos[0], pos[1]):
+                #if grid not empty then allow play
+                if exo.CheckEmptyByXY(board, pos[0], pos[1]):
                     board[pos[1]][pos[0]] = str(turn)
 
                     UpdateBoard(board)
@@ -96,6 +103,15 @@ if __name__ == "__main__":
                         txt = FONT.render(f'{["Blue", "Red"][int(turn)]} Win!', True, WHITE, BLACK)
                         rect = txt.get_rect(center = (SCREEN_SIZE / 2 + OFFSET, SCREEN_SIZE / 2 + OFFSET))
                         SCREEN.blit(txt, rect)
+                    elif exo.CheckGameEnd(board):
+                        print(win)
+                        txt = FONT.render("It\'s a Tie", True, WHITE, BLACK)
+                        rect = txt.get_rect(center = (SCREEN_SIZE / 2 + OFFSET, SCREEN_SIZE / 2 + OFFSET))
+                        SCREEN.blit(txt, rect)
+                    
+
+                    if agent.side == abs(turn - 1):
+                        print(agent.FindBestMove(board))
 
                     turn = abs(turn - 1)
 
